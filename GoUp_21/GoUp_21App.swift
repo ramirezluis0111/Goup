3b448dea488652120b56ac5163d6dbd2881993ea
LuisRamirez <ramirezluis0111@gmail.com>
2021-05-05 11:45:39 -0300

    Initial Commit

---
+++ GoUp_21/GoUp_21App.swift
@@ -0,0 +1,17 @@
+//
+//  GoUp_21App.swift
+//  GoUp_21
+//
+//  Created by Ramirez Luis on 05/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct GoUp_21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoUp_21Tests/GoUp_21Tests.swift
@@ -0,0 +1,33 @@
+//
+//  GoUp_21Tests.swift
+//  GoUp_21Tests
+//
+//  Created by Ramirez Luis on 05/05/2021.
+//
+
+import XCTest
+@testable import GoUp_21
+
+class GoUp_21Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
